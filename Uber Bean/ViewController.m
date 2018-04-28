b06adc39d1e38b6cd7720c3d2c6dc1a7d16911ce
Raman Singh <rami88@ymail.com>
2018-04-28 10:09:38 -0700

    Still working on deque reusable pin view

--- Uber Bean/ViewController.m
+++ Uber Bean/ViewController.m
@@ -40,6 +40,7 @@
     region.span = span;
     region.center = location;
     [self.mapView setRegion:region animated:YES];
+    NSLog(@"current location is %@", self.locationManager.location);
     
     NetworkManager *myNetMan = [NetworkManager new];
     [myNetMan dictFromString:@"cafe" andLatitude:self.location.coordinate.latitude andLongitude:self.location.coordinate.longitude completionHandler:^(NSDictionary *returnDict, NSError *error) {
@@ -48,7 +49,7 @@
         
         
         
-        
+        int i = 16;
         for (int i=0; i<self.allPlaces.count; i++) {
             Cafe *thisCafe = [Cafe new];
             NSDictionary *thisDict = [self.allPlaces objectAtIndex:i];
@@ -71,32 +72,36 @@
             NSURL *thisURL = [NSURL URLWithString:thisCafe.image_url];
             [self loadImageWithURLandReturnImage:thisURL forCafeObject:thisCafe];
             [self.listings addObject:thisCafe];
-            
+        NSLog(@"all places is %lu", self.allPlaces.count);
+                  
+        NSLog(@"place name is %@", thisCafe.name);
+                  
+                  
+                  
+        NSString *latitude = thisCafe.coordinates[@"latitude"];
+        NSString *longitude = thisCafe.coordinates[@"longitude"];
+        double b = [latitude doubleValue];
+        double c = [longitude doubleValue];
+
+        MKPointAnnotation *myAnnotation = [[MKPointAnnotation alloc]init];
+
+        myAnnotation.coordinate = CLLocationCoordinate2DMake(b, c);
+        [myAnnotation setTitle:thisCafe.name];
+        [myAnnotation setSubtitle:thisCafe.phone];
+        [myAnnotation setSubtitle:@"come over bitches"];
+        [self.mapView addAnnotation: myAnnotation];
         }//forLoop
+     
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+//        NSLog(@"%@", thisCafe.coordinates);
         
         
         
     }];//completion
     
-    
+ 
+
     
     
     
@@ -146,9 +151,32 @@
     
     [downloadTask resume];
     
-}
+}//loadImage
 
-
+//- (MKAnnotationView *)mapView:(MKMapView *)mapView
+//            viewForAnnotation:(id <MKAnnotation>)annotation
+//{
+//    MKPinAnnotationView *pinView = (MKPinAnnotationView*)[mapView dequeueReusableAnnotationViewWithIdentifier:@"myId"];
+//
+//    if (!pinView)
+//    {
+//        // If an existing pin view was not available, create one.
+//        pinView = [[MKPinAnnotationView alloc] initWithAnnotation:annotation reuseIdentifier:@"myId"];
+//        pinView.canShowCallout = YES;
+//        pinView.pinTintColor = [UIColor greenColor];
+//        pinView.image = [UIImage imageNamed:@"pin.png"];
+//
+//        UIButton* rightButton = [UIButton buttonWithType:UIButtonTypeDetailDisclosure];
+//        pinView.rightCalloutAccessoryView = rightButton;
+//
+//        // Add an image to the left callout.
+//        UIImageView *iconView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"lhlLogo.png"]];
+//        pinView.leftCalloutAccessoryView = iconView;
+//
+//
+//    }
+//    return pinView;
+//}
 
 
 
